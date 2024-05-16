5000610cfe11edbcc89c6389b20a6871feba1387
Yanjiiii <163888400+Yanjiiii@users.noreply.github.com>
2024-05-16 08:51:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let webWeaver = "<b>web weaver</b>";
+let alchemistOfTheBrowser = "<b>alchemist of the browser</b>";
+let CSScloak = "<b>CSS cloak</b>";
+let JavaScriptspells = "<b>JavaScript spells</b>";
+let debuggingSpectacles = "<b>debugging spectacles</b>";
+let responsiveSea = "<b>responsive sea</b>";
+let SEOwhisperer = "<b>SEO whisperer</b>";
+let developerName = "<b>Gian</b>";
+
+let sentence1 = `In the realm of ones and zeros, where pixels dance and code unfurls, there resides a silent sorcerer, a ${webWeaver} with dreams and pearls.`;
+let sentence2 = `${developerName}, the ${alchemistOfTheBrowser}, wields his keyboard like a wand. He conjures pages from thin air, painting landscapes with lines so fond.`;
+let sentence3 = `Next, he dons his ${CSScloak}, styling elements with flair and grace. Colors bloom, fonts sing, layouts evoke, a visual ballet in this digital space.`;
+let sentence4 = `But wait, the magic deepens still, ${developerName} summons ${JavaScriptspells}. Interactivity blooms at his will, forms validate, animations propel.`;
+let sentence5 = `Yet, bugs creep in like shadows dark, ${developerName} dons his ${debuggingSpectacles}. Chasing glitches through the code's arc, he tames wild loops and elusive variables.`;
+let sentence6 = `Across screens, devices, and dimensions, ${developerName} sails on the ${responsiveSea}. Media queries guide his intentions, adapting layouts with fluidity.`;
+let sentence7 = `In the labyrinth of search engines, ${developerName} becomes an ${SEOwhisperer}. Meta tags, keywords, backlink strings, he crafts pathways to digital wanderers.`;
+let sentence8 = `And when the moon kisses the horizon, ${developerName} unfurls his creation to the world. Deployments like shooting stars, arisen, his website sails, its sails unfurled.`;
+let sentence9 = `So here's to ${developerName}, the web developer, a poet of pixels, a composer of clicks. In the binary symphony, he's the reveler, weaving dreams into the digital matrix.`;
+
+let contentDiv = document.getElementById("content");
+
+contentDiv.innerHTML += "<p><i>" + sentence1 + "</i></p>";
+contentDiv.innerHTML += "<p><i>" + sentence2 + "</i></p>";
+contentDiv.innerHTML += "<p><i>" + sentence3 + "</i></p>";
+contentDiv.innerHTML += "<p><i>" + sentence4 + "</i></p>";
+contentDiv.innerHTML += "<p><i>" + sentence5 + "</i></p>";
+contentDiv.innerHTML += "<p><i>" + sentence6 + "</i></p>";
+contentDiv.innerHTML += "<p><i>" + sentence7 + "</i></p>";
+contentDiv.innerHTML += "<p><i>" + sentence8 + "</i></p>";
+contentDiv.innerHTML += "<p><i>" + sentence9 + "</i></p>";
